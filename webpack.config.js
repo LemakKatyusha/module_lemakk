98bab73ff64e53734867dee0cfe7257f55fd658c
LemakKatyusha <lemak.kateryna@student.uzhnu.edu.ua>
2024-10-13 19:04:19 +0300

    1.1
    1.2
    
    2

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,65 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import { fileURLToPath } from 'url';
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  return {
-    entry: "./src/index.jsx",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: isProduction ? "[name].[contenthash].js" : "bundle.js",
-      clean: true,
-    },
-    mode: isProduction ? "production" : "development",
-    devtool: isProduction ? "source-map" : "inline-source-map",
-    devServer: {
-      static: path.resolve(__dirname, "dist"),
-      port: 3000,
-      open: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: "babel-loader",
-        },
-        {
-          test: /\.css$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-            "css-loader",
-          ],
-        },
-      ],
-    },
-    optimization: {
-      minimize: isProduction,
-      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-    },
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-        minify: isProduction && {
-          collapseWhitespace: true,
-          removeComments: true,
-        },
-      }),
-      isProduction &&
-        new MiniCssExtractPlugin({
-          filename: "[name].[contenthash].css",
-        }),
-    ].filter(Boolean),
-  };
+export default (env, argv) => {
+	const isProduction = argv.mode === 'production';
+
+	return {
+		entry: './src/index.jsx',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+			clean: true,
+		},
+		mode: isProduction ? 'production' : 'development',
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			static: path.resolve(__dirname, 'dist'),
+			port: 3000,
+			open: true,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					use: 'babel-loader',
+				},
+				{
+					test: /\.css$/,
+					use: [
+						isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+						'css-loader',
+					],
+				},
+			],
+		},
+		optimization: {
+			minimize: isProduction,
+			minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+		},
+		resolve: {
+			extensions: ['.js', '.jsx'],
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './public/index.html',
+				minify: isProduction && {
+					collapseWhitespace: true,
+					removeComments: true,
+				},
+			}),
+			isProduction &&
+				new MiniCssExtractPlugin({
+					filename: '[name].[contenthash].css',
+				}),
+		].filter(Boolean),
+	};
 };
